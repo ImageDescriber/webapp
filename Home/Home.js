ffa4afa4d60b2a8d4532a7d0a3add9d897f5adfa
KarlPineau <karl.pineau@ens-lyon.fr>
2017-06-24 14:26:48 +0200

    Version alpha 2

--- Home/Home.js
+++ Home/Home.js
@@ -1,37 +1,59 @@
 app.controller("HomeCtrl", function ($scope, $http, $sce, $timeout, $parse, $cookies, $cookieStore, $route) {
     /* -- Scope and variables definition -- */
-    $scope.loading = true;
-    $scope.newDepictValue = "";
-    $scope.depicts = [];
-    $scope.previousDepicts = [];
-    $scope.loadThanks = false;
+    var server = "http://localhost:8888/ImageDescription/api/web/app_dev.php"; //http://imagedescription.histoiredelart.fr/api/web
 
-    $scope.search = {};
+    /* Variables */
+    $scope.page = {
+        loading: true,
+        loadThanks: false,
+        translation: YAML.load('web/translation.yml')['en']
+    };
+    $scope.depict = {
+        newDepictValue: "",
+        depicts: [],
+        wdtDepicts: [],
+        wdtDepictsConstruction: [],
+        wdtDepictsLength: 0
+    };
+    $scope.search = {
+        language: "en",
+        languageDefinition: false,
+        searchStr: "",
+        id: "searchInput",
+        pause: 100,
+        selectedObject: "selectedObject",
+        url: "https://www.wikidata.org/w/api.php?action=wbsearchentities&language=en&format=json&search=",
+        inputclass: "form-control",
+        minLength: 1
+    };
+    $scope.item = {
+        qwd: null,
+        image: null,
+        label: null,
+        description: null,
+        creator: null,
+        date_creation: null,
+        genre: null,
+        collection: null,
+        url: null
+    };
+    /* Variables */
+
     /* Language modal management */
-    if($cookieStore.get('id_histoiredelart_language') === undefined) {
-        $scope.search.language = "en";
-        $('#modalLanguage').modal('show');
-    } else {
+    if($cookieStore.get('id_histoiredelart_language') !== undefined) {
+        $scope.search.languageDefinition = true;
         $scope.search.language = $cookieStore.get('id_histoiredelart_language');
+        $scope.search.url = "https://www.wikidata.org/w/api.php?action=wbsearchentities&language="+$scope.search.language+"&format=json&search=";
+        $scope.page.translation = YAML.load('web/translation.yml')[$scope.search.language];
     }
-    $scope.active = function (id) {
-        $('#' + id).addClass('active');
+    $scope.setLanguage = function (language) {
+        $scope.search.languageDefinition = true;
+        $scope.search.language = language;
+        $scope.page.translation = YAML.load('web/translation.yml')[$scope.search.language];
         $scope.search.url = "https://www.wikidata.org/w/api.php?action=wbsearchentities&language="+$scope.search.language+"&format=json&search=";
         $cookieStore.put('id_histoiredelart_language', $scope.search.language);
     };
-    $scope.closeModalLanguage = function() {
-        $('#modalLanguage').modal('hide');
-    };
     /* Language modal management */
-
-    $scope.search.searchStr = "";
-    $scope.search.id = "searchInput";
-    $scope.search.placeholder = "\"Jésus\", \"femme\", \"chapeau\", \"forêt\"...";
-    $scope.search.pause = 100;
-    $scope.search.selectedObject = "selectedObject";
-    $scope.search.url = "https://www.wikidata.org/w/api.php?action=wbsearchentities&language="+$scope.search.language+"&format=json&search=";
-    $scope.search.inputclass = "form-control";
-    $scope.search.minLength = 1;
 
     /* HomeTop management */
     if($cookieStore.get('id_histoiredelart_topmanagement') !== undefined) {
@@ -41,13 +63,9 @@ app.controller("HomeCtrl", function ($scope, $http, $sce, $timeout, $parse, $coo
     }
     $scope.topManagement = function() {
         if($scope.topMangement === "up") {
-            $('#topHome p').hide();
-
             $scope.topMangement = "down";
             $cookieStore.put('id_histoiredelart_topmanagement', 'down');
         } else if($scope.topMangement === "down") {
-            $('#topHome p').hide();
-
             $scope.topMangement = 'up';
             $cookieStore.put('id_histoiredelart_topmanagement', 'up');
         }
@@ -55,8 +73,7 @@ app.controller("HomeCtrl", function ($scope, $http, $sce, $timeout, $parse, $coo
     /* HomeTop management */
 
     /* Get item */
-    $http.get('http://localhost:8888/ImageDescriber/api/web/app_dev.php/entities?random=true').then(function (response) {
-        console.log(response.data);
+    $http.get(server+'/entities?random=true').then(function (response) {
         $scope.item = response.data[0];
 
         var url = "http://www.wikidata.org/w/api.php?action=wbgetentities&ids=Q"+$scope.item.qwd+"&format=json";
@@ -64,47 +81,82 @@ app.controller("HomeCtrl", function ($scope, $http, $sce, $timeout, $parse, $coo
 
         $http.jsonp(trustedUrl, {jsonpCallbackParam: 'callback'})
             .then(function(response){
-                console.log(response.data);
                 $scope.wdtItem = response.data['entities']["Q"+$scope.item.qwd];
+                generateInfoItem();
             });
 
-        $scope.loading = false;
+        $scope.page.loading = false;
     });
+    function generateInfoItem() {
+        console.log($scope.wdtItem);
+        $scope.item.label = getProperty($scope.wdtItem.labels);
+        $scope.item.description = getProperty($scope.wdtItem.descriptions);
+        if($scope.wdtItem.claims["P136"] !== undefined) {
+            /* P136 == Genre */
+            $scope.item.genre = getProperty($scope.wdtItem.claims["P136"]);
+        }
+        if($scope.wdtItem.claims["P571"] !== undefined) {
+            /* P571 == Date of creation */
+            $scope.item.date_creation = getProperty($scope.wdtItem.claims["P571"]);
+        }
+        if($scope.wdtItem.claims["P170"] !== undefined) {
+            /* P170 == Creator */
+            $scope.item.creator = getProperty($scope.wdtItem.claims["P170"]);
+        }
+        if($scope.wdtItem.claims["P195"] !== undefined) {
+            /* P195 == Collection */
+            $scope.item.collection = getProperty($scope.wdtItem.claims["P195"]);
+        }
+        if($scope.wdtItem.claims["P973"] !== undefined) {
+            /* P973 == Described at */
+            $scope.item.url = getProperty($scope.wdtItem.claims["P973"]);
+        }
+
+        if($scope.wdtItem.claims["P180"] !== undefined) {
+            /* P180 == Depicts */
+            $scope.depict.wdtDepictsLength = $scope.wdtItem.claims["P180"].length;
+            loadClaims($scope.wdtItem.claims["P180"]);
+        }
+    }
     /* Get item */
 
+    /* Actions */
     $scope.newDepict = function() {
-        console.log($scope.search.selectedObject);
-        $scope.depicts.push($scope.search.selectedObject);
-        console.log($scope.depicts);
-
+        $scope.depict.depicts.push($scope.search.selectedObject);
         $scope.search.searchStr = "";
     };
 
     $scope.removeDepict = function(dDepict) {
-        console.log(dDepict);
-        console.log($scope.depicts);
-        for(depict in $scope.depicts) {
-            if(dDepict["qwd"] === $scope.depicts[depict]["qwd"]) {
-                console.log('ok');
-                $scope.depicts.splice(depict, 1);
+        for(depict in $scope.depict.depicts) {
+            if(dDepict["qwd"] === $scope.depict.depicts[depict]["qwd"]) {
+                $scope.depict.depicts.splice(depict, 1);
             }
         }
     };
 
     $scope.persist = function() {
         $scope.loadThanks = true;
-        if($scope.depicts.length > 0) {
+        if($scope.depict.depicts.length > 0) {
             var qwds = [];
-            for(depict in $scope.depicts) {
-                qwds.push($scope.depicts[depict]["qwd"]);
+            for(depict in $scope.depict.depicts) {
+                qwds.push($scope.depict.depicts[depict]["qwd"]);
             }
-            $http.patch('http://localhost:8888/ImageDescriber/api/web/app_dev.php/entities/'+$scope.item.id, {"listDepicts": qwds}).then(function (response) {
-                console.log(response.data);
+            $http.patch(server+'/entities/'+$scope.item.id, {"listDepicts": qwds}).then(function (response) {
                 $route.reload();
             });
         }
     };
 
+    $scope.nothingToAdd = function() {
+        $scope.loadThanks = true;
+        $route.reload();
+    };
+
+    $scope.pass = function() {
+        $scope.loadThanks = true;
+        $route.reload();
+    };
+    /* Actions */
 
     /* AnguComplete Fork */
     $scope.processResults = function(responseData, str) {
@@ -178,5 +230,77 @@ app.controller("HomeCtrl", function ($scope, $http, $sce, $timeout, $parse, $coo
             }, $scope.search.pause);
         }
     });
+    /* AnguComplete Fork */
 
+    /* Function: depicts */
+    function loadClaims(claimP180) {
+        for(var iClaim in claimP180) {
+            var entry = claimP180[iClaim];
+
+            if(entry.mainsnak.datavalue.value.id !== undefined) {
+                var url = "http://www.wikidata.org/w/api.php?action=wbgetentities&ids="+entry.mainsnak.datavalue.value.id+"&format=json";
+                var trustedUrl = $sce.trustAsResourceUrl(url);
+
+                $http.jsonp(trustedUrl, {jsonpCallbackParam: 'callback'})
+                    .then(function (response) {
+                        //console.log(response.data['entities']);
+                        $scope.depict.wdtDepictsConstruction.push(response.data['entities']);
+                        encodeClaims();
+                    });
+            }
+        }
+    }
+    function encodeClaims() {
+        //console.log($scope.depict.wdtDepictsConstruction);
+        if ($scope.depict.wdtDepictsConstruction.length === $scope.depict.wdtDepictsLength) {
+            for(var wdtDepictId in $scope.depict.wdtDepictsConstruction) {
+                var wdtDepict = $scope.depict.wdtDepictsConstruction[wdtDepictId];
+                for(var itemQwd in wdtDepict) {
+                    var item = wdtDepict[itemQwd];
+                    $scope.depict.wdtDepicts.push({qwd: itemQwd, label: getClaimLabel(item)});
+                }
+            }
+        }
+    }
+    function getClaimLabel(item) {
+        var labels = item.labels;
+        if(labels[$scope.search.language] !== undefined) {
+            return item.labels[$scope.search.language].value;
+        } else if(labels.en !== undefined) {
+            return item.labels.en.value;
+        } else if(labels.fr !== undefined) {
+            return item.labels.fr.value;
+        } else if(labels.it !== undefined) {
+            return item.labels.it.value;
+        } else if(labels.de !== undefined) {
+            return item.labels.de.value;
+        } else if(labels.nl !== undefined) {
+            return item.labels.nl.value;
+        } else if(labels.es !== undefined) {
+            return item.labels.es.value;
+        }
+    }
+    /* Function: depicts */
+
+
+    /* Object Browser for Wikidata */
+    function getProperty(property) {
+        if(property[$scope.search.language] !== undefined) {
+            return property[$scope.search.language].value;
+        } else if(property.en !== undefined) {
+            return property.en.value;
+        } else if(property.fr !== undefined) {
+            return property.fr.value;
+        } else if(property.it !== undefined) {
+            return property.it.value;
+        } else if(property.de !== undefined) {
+            return property.de.value;
+        } else if(property.nl !== undefined) {
+            return property.nl.value;
+        } else if(property.es !== undefined) {
+            return property.es.value;
+        } else {
+            return "";
+        }
+    }
 });
